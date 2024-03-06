b64f41a9ead0ee017969010d8970001651d76a02
bedwards2023 <143897958+bedwards2023@users.noreply.github.com>
2024-03-06 17:44:34 -0600

    firstdraft push

---
+++ EdwardsProjectPart1.py
@@ -0,0 +1,10 @@
+"""
+
+Name:Brittany Edwards
+Date:03/06/2024
+Assignment:Module 9: Project - Part 1
+Due Date:03/17/2024
+About this project: This is the Dash portion of Mod 7, this is the drop down dash site and the slider portion
+All work below was performed by Brittany Edwards
+
+"""
