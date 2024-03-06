6d2eb7bec07a507e1796b5683739f2565032e125
bedwards2023 <143897958+bedwards2023@users.noreply.github.com>
2024-03-06 17:51:32 -0600

    Getting set up

--- EdwardsProjectPart1.py
+++ EdwardsProjectPart1.py
@@ -8,3 +8,17 @@ About this project: This is the Dash portion of Mod 7, this is the drop down das
 All work below was performed by Brittany Edwards
 
 """
+
+#My two questions I'm attempting to answer
+print("The two questions we are attempting to solve are: Is there a correlation between poverty rates (2011-2021) and crime rates in different states?")
+print("How does the level of poverty in a state relate to its incarceration rates?")
+
+'''
+I'm using three data sets:
+1. Crime and Incarceration in the United States https://www.kaggle.com/datasets/christophercorrea/prisoners-and-crime-in-united-states
+2. Poverty estimates https://www.kaggle.com/datasets/marshuu/poverty-estimates 
+3. Statewise crime in USA - K means Clustering https://www.kaggle.com/datasets/vikramamin/statewise-crime-in-usa-k-means-clustering
+
+For each state and each year between 2011-2017 I will be looking at the poverty estimates for each state, as well as crime and incarceration rates. 
+Are incarceration rates correlated to poverty rates? How do states differ in crime rates and poverty rates? 
+'''
