ca4cc533a3df227e899f461f0b55b4d7ef3108ad
bedwards2023 <143897958+bedwards2023@users.noreply.github.com>
2024-03-13 19:22:46 -0500

    New project files

--- EdwardsProjectPart1.py
+++ EdwardsProjectPart1.py
@@ -10,20 +10,43 @@ All work below was performed by Brittany Edwards
 """
 
 #My two questions I'm attempting to answer
-print("The two questions we are attempting to solve are: Does lifestyle influence lung cancer? ")
-print("How does the level of poverty in a state relate to its incarceration rates?")
+print("What lifestyle factors are significantly associated with the presence of lung cancer?")
+print("Do certain lifestyle factors change the stage of the cancer?")
 
 '''
 I'm using three data sets:
-1. Crime and Incarceration in the United States https://www.kaggle.com/datasets/christophercorrea/prisoners-and-crime-in-united-states
-2. Poverty estimates https://www.kaggle.com/datasets/marshuu/poverty-estimates 
-3. Statewise crime in USA - K means Clustering https://www.kaggle.com/datasets/vikramamin/statewise-crime-in-usa-k-means-clustering
-
-For each state and each year between 2011-2017 I will be looking at the poverty estimates for each state, as well as crime and incarceration rates. 
-Are incarceration rates correlated to poverty rates? How do states differ in crime rates and poverty rates? 
+1.Cancer Prediction Dataset https://www.kaggle.com/datasets/fdcellat/cancer-prediction-dataset
+2.Lung Cancer Prediction https://www.kaggle.com/datasets/thedevastator/cancer-patients-and-air-pollution-a-new-link
+3.Smoking related lung cancers https://www.kaggle.com/datasets/raddar/smoking-related-lung-cancers
 '''
 
-a = [1,2,3]
-b=a
-a += [4,5]
-print(b)
+import pandas as pd
+import xlrd
+import openpyxl
+
+file2 = r"cancer patient data sets.csv"
+file3=r"lung_cancer.csv"
+
+'''
+For each file
+Using python, create a script that imports the data set and evaluate the central tendencies and variation of attributes. '''
+cancerPredictionDS = pd.read_excel(r'Cancer_prediction_dataset.xlsx', engine='openpyxl')
+cancerPatientsDS = pd.read_csv(file2)
+lungcancerDS = pd.read_csv(file3)
+
+print(cancerPredictionDS.describe())
+print(cancerPatientsDS.describe())
+print(lungcancerDS.describe())
+
+
+'''
+List the data sets and attributes that could be used to answer each of the 2 questions you identified, namely, which attribute are you going to select to be the "y" attribute (value your model will predict), 
+and which attributes are you going to select to be your "X" attributes (the attributes you will base your prediction upon). 
+Note: You must have at least 15 X attributes and some of these attributes must be strings (i.e., text input)
+'''
+print("My y will be in the dataset cancerPredictionDS. It will be either a Yes or No if the person has lung cancer. ")
+print("I will using the following attributes from the cancerPatientsDS data frame to answer my questions: ")
+print(cancerPatientsDS.columns[4:25].tolist())
+print("As well as these attributes from the cancerPredictionDS:")
+print(cancerPredictionDS.columns[2:10].tolist())
+print("Then for my second question my Y will be the stage of the cancer, in lungcancerDS, also using the same attributes as above. ")
